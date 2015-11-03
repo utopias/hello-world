1d23e0cb3b9533ff5f32946a5a10db0e11ca9061
Jason Pero <jason.pero@ignitionone.com>
2015-11-03 16:22:28 -0500

    Added logging and config #20 #time 1h

--- app/server/app.js
+++ app/server/app.js
@@ -1,1 +1,7 @@
-console.log('Hello command line!');
+var express = require('express');
+var app = express();
+var config = require('./helpers/config');
+var logger = require('./helpers/logger')();
+
+logger.info("Hello command line via bunyan!");
+logger.info("Config:", config.get('app'));
