e4cd0dc01b1773ee0c169d8898f9157dad0dcf93
Nate Clark <nathan.clark@ignitionone.com>
2015-11-04 16:36:57 -0500

    Added a hello world route

--- app/server/app.js
+++ app/server/app.js
@@ -1,1 +1,25 @@
-console.log('Hello command line!'); // eslint-disable-line no-console
+var express = require('express');
+var app = express();
+
+var server;
+var port = process.env.PORT;
+var host = process.env.HOST;
+
+app.get('/', function(req, res) {
+  return res.send('Hello world!');
+});
+
+server = app.listen(port, host, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('%s@%s listening at http://%s:%s on Node', // eslint-disable-line
+    process.env.npm_package_name,
+    process.env.npm_package_version,
+    host,
+    port,
+    process.version
+  );
+});
+
+module.exports = server;
