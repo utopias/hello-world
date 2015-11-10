4bf94e7320858d1132e6750a8eb1dbc6239dff49
Nate Clark <nathan.clark@ignitionone.com>
2015-11-09 21:13:20 -0500

    Added top level spec files

---
+++ tests/unit/unit.spec.js
@@ -0,0 +1,9 @@
+// var configSpecs = require('./config');
+// var controllerSpecs = require('./controllers');
+// var routeSpecs = require('./routes');
+// var helperSpec = require('./helpers');
+
+// describe('Config', configSpecs);
+// describe('Controllers', controllerSpecs);
+// describe('Routes', routeSpecs);
+// describe('Helpers', helperSpec);
